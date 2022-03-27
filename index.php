e2e35e3314ff8394f167f24d86e426d72407801a
Yassine Berqiqch <yassine.berqiqch@gmail.com>
2022-03-27 22:05:39 +0100

    aaa

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+    <title>My Portfolio - Redirection</title>
+    <meta http-equiv="refresh" content="0;URL=http://berqiqch.eu5.net">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+  </head>
+</head>
+<body>
+    
+</body>
+</html>
