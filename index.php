8330cdfab5842e59a1b281a3daea0712a50b2857
Yassine Berqiqch <yassine.berqiqch@gmail.com>
2022-03-27 22:09:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Portfolio - Redirection</title>
-    <meta http-equiv="refresh" content="0;URL=http://berqiqch.eu5.net">
-    <link rel="icon" href="./favicon.ico" type="image/x-icon">
-</head>
-</html>
+<?
+header("location: http://berqiqch.eu5.net");
+?>
